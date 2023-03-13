e18645676298a1605515b15e1c00520f83c9eccd
PFonsecaFV <pfonsecafv@gmail.com>
2023-03-13 18:02:44 -0300

    Exercício 06 - Concluído

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,7 +4,23 @@ import '../styles/wallet.css';
 class Table extends Component {
   render() {
     return (
-      <div className="wallet-table " />
+      <div className="wallet-table ">
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     );
   }
 }
